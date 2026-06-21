d10f0eb4a8cfb9127e65fbabc984027f6ecdee7b
Winnerito <Patcharakasit.pk@gmail.com>
2026-06-21 18:51:19 +0700

    ยกเลิกการใช้ฟังชั่นซ่อน กล่อง portfolio เเละ ปรับ ฟอร์นใหม่

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById('togglePortfolioBtn');
-const wrapper = document.getElementById('projectWrapper');
-
-toggleBtn.addEventListener('click', () => {
-  // ลองเขียนเอง: สลับ class 'is-open' แล้วเปลี่ยนข้อความปุ่มตามสถานะ
-});
